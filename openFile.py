d230f57aff1c42b384b26de1e02cdbdab0fc3925
AnhVu8499 <98485978+AnhVu8499@users.noreply.github.com>
2022-12-02 17:11:44 -0800

    updated openFIle

--- openFile.py
+++ openFile.py
@@ -3,47 +3,20 @@
 
 import csv
 import pandas as pd
+import time
+start_time = time.time()
 
-fname = input("Enter file name: ")
-method = input("Use pandas ? (yes/no) ")
-#read file without pandas
-def loadFile(fname):
-    count = 0
-    with open(fname, 'r') as file:
+#read file 
+def loadFile(filename):
+    with open(filename, 'r') as file:
+        count = 0
         reader = csv.reader(file, delimiter = ',')
         cols = len(next(reader))
         for row in reader:
-            print(row)
-            count+=1
-    print ("Total columns:")
-    print (cols)
-    print ("Total rows:")
-    print (count)
-    
-"""
-class nameSwitch:
-    def colName(self, colNum):
-        default = "Done"
-        return getattr(self, 'case_' + str(colNum), lambda:default)()
-    def case_1(self):
-        return "MONTH"
-    def case_2(self):
-        return "DAY_OF_WEEK"
-    def case_3(self):
-        return "DEP_DEL15"
-mySwitch = nameSwitch()
-"""
+            count += 1
+        print("--- %s seconds ---" % (time.time() - start_time), end =" ")
+        print("Total columns read: ", cols) 
+        print("--- %s seconds ---" % (time.time() - start_time), end =" ")
+        print("Total rows read: ", count)
 
-#read with pandas
-def readByPandas(fname):
-    df = pd.read_csv(fname)
-    print(df)
 
-    #colNum = input("Which column do you want to show? ")
-    #print(mySwitch.colName(colNum))
-    #print (df[['MONTH', 'DAY_OF_WEEK']])
-
-if method == 'yes':
-    readByPandas(fname)
-else:
-    loadFile(fname)
